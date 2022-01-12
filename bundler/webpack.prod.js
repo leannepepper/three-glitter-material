e4c8001fe3499ebce7b35b8f5d10c26307e4bf24
Leanne Werner <leanne.werner.lw@gmail.com>
2022-01-12 12:19:41 -0800

    wip

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -1,14 +1,14 @@
 const { merge } = require('webpack-merge')
 const commonConfiguration = require('./webpack.common.js')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require('path')
 
-module.exports = merge(
-    commonConfiguration,
-    {
-        mode: 'production',
-        plugins:
-        [
-            new CleanWebpackPlugin()
-        ]
-    }
-)
+module.exports = merge(commonConfiguration, {
+  mode: 'production',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/glitter-material'
+  },
+  plugins: [new CleanWebpackPlugin()]
+})
