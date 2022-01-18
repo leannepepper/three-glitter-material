0b5f49d9fcb10e44a5a112deb15764ff2459b931
Leanne Werner <leanne.werner.lw@gmail.com>
2022-01-18 11:56:20 -0800

    webpack config changes

--- packages/site/bundler/webpack.dev.js
+++ packages/site/bundler/webpack.dev.js
@@ -12,26 +12,23 @@ module.exports = merge(commonConfiguration, {
   devServer: {
     host: '0.0.0.0',
     port: portFinderSync.getPort(8080),
-    contentBase: './dist',
-    watchContentBase: true,
+    // contentBase: './dist',
+    // watchContentBase: true,
     open: true,
-    https: false,
-    useLocalIp: true,
-    disableHostCheck: true,
-    overlay: true,
-    noInfo: true,
-    after: function (app, server, compiler) {
-      const port = server.options.port
-      const https = server.options.https ? 's' : ''
-      const localIp = ip.v4.sync()
-      const domain1 = `http${https}://${localIp}:${port}`
-      const domain2 = `http${https}://localhost:${port}`
+    https: false
+    // useLocalIp: true
+    // disableHostCheck: true
+    // overlay: true
+    // noInfo: true
+    // after: function(app, server, compiler)
+    // {
+    //     const port = server.options.port
+    //     const https = server.options.https ? 's' : ''
+    //     const localIp = ip.v4.sync()
+    //     const domain1 = `http${https}://${localIp}:${port}`
+    //     const domain2 = `http${https}://localhost:${port}`
 
-      console.log(
-        `Project running at:\n  - ${infoColor(domain1)}\n  - ${infoColor(
-          domain2
-        )}`
-      )
-    }
+    //     console.log(`Project running at:\n  - ${infoColor(domain1)}\n  - ${infoColor(domain2)}`)
+    // }
   }
 })
